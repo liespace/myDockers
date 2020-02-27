b774c1f9fbc014f1847e742bb6fcca5976b583a8
Gabel Liemann <troubleli233@gmail.com>
2020-02-28 00:44:29 +0800

    modify some code

--- carla/run.sh
+++ carla/run.sh
@@ -1,2 +1,2 @@
-docker run --runtime=nvidia -it --name carla --mount type=volume,source=trouble,target=/home/trouble glep:carla-0.9.6 bash
+docker run --runtime=nvidia -it --name carla --mount type=volume,source=trouble,target=/home/trouble carlasim/carla:0.9.6 bash
 # ./CarlaUE4.sh -quality-level=Low
