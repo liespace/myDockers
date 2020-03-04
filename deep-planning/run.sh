21d09d8e13498834b559160863e008709b56eff2
Gabel Liemann <troubleli233@gmail.com>
2020-03-04 08:37:53 +0800

    add deep-planning-ros-base docker

--- deep-planning/run.sh
+++ deep-planning/run.sh
@@ -1,2 +1,1 @@
 docker run --runtime=nvidia -it --name deep-planning --mount type=volume,source=trouble,target=/home/trouble -p 6008:6006 glep:deep-planning bash
-#docker run --runtime=nvidia -it --name deep-planning --mount type=volume,source=trouble,target=/home/trouble -p 6008:6006 glep:deep-planning bash
